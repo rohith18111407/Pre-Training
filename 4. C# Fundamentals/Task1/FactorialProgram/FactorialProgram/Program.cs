d0f0d2cef7f53b05daee79e05302213dc2ead1d0
Rohith M <108167442+rohith18111407@users.noreply.github.com>
2025-04-03 10:00:53 +0530

    Complete Task 1,2

---
+++ 4. C# Fundamentals/Task1/FactorialProgram/FactorialProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive integer: ");
+        string input = Console.ReadLine(); // Read user input
+        if (int.TryParse(input, out int number) && number >= 0)
+        {
+            long factorial = CalculateFactorial(number);
+            Console.WriteLine($"Factorial of {number} is: {factorial}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter a positive integer.");
+        }
+    }
+
+    static long CalculateFactorial(int n)
+    {
+        long factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
---
+++ 4. C# Fundamentals/Task2/SimpleObjectOrientedProgramming/SimpleObjectOrientedProgramming/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Person
+{
+    // Properties (Encapsulation)
+    // get allows retrieving the val of the property
+    // set allows assigning a new value to property
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // Constructor
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Method to introduce the person
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating Person objects
+        Person person1 = new Person("Rohith", 21);
+        Person person2 = new Person("Santhosh", 21);
+        Person person3 = new Person("Preethi", 25);
+
+        // Calling the Introduce method for each person
+        person1.Introduce();
+        person2.Introduce();
+        person3.Introduce();
+    }
+}
